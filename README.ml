d4a31f2e0f2a134cd97005c38f9cc7914909f7fb
eric <ericwang42@outlook.com>
2022-03-18 00:14:25 -0700

    the beginning and the end

--- README.ml
+++ README.ml
@@ -1,1 +1,8 @@
-say hello to my lil git
+According to the standard Big Bang model, the universe was born during a period of inflation that began about 13.8 billion years ago. 
+Like a rapidly expanding balloon, it swelled from a size smaller than an electron to nearly its current size within a tiny fraction of a second
+
+Initially, the universe was permeated only by energy. Some of this energy congealed into particles, which assembled into light atoms like hydrogen and helium. 
+These atoms clumped first into galaxies, then stars, inside whose fiery furnaces all the other elements were forged
+
+This is the generally agreed-upon picture of our universe's origins as depicted by scientists. It is a powerful model that explains many of the things scientists see when they look up in the sky, 
+such as the remarkable smoothness of space-time on large scales and the even distribution of galaxies on opposite sides of the universe.
